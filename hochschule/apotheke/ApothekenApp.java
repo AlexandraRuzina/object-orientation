d550c2a5d4c6ea6ebf670fd8ef03cd9a53ceb736
Prof. Dr. Verena Majuntke <verena.majuntke+github@htw-berlin.de>
2023-12-12 14:34:16 +0100

    ApothekeApp hinzugefügt

---
+++ hochschule/apotheke/ApothekenApp.java
@@ -0,0 +1,13 @@
+package apotheke;
+
+public class ApothekenApp {
+
+    Apotheke apo = new Apotheke("Berlin", "Harald Hecht");
+    //rosenApotheke.gebeVerfuegbareMedikamenteAus();
+
+    Medikament ibuprofen = new Medikament("Ibuprofen", 1234, "Ratiopharm");
+    
+
+
+    
+}
